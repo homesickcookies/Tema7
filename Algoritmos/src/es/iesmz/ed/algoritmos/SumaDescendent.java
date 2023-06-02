abc06c408bcc208346021634c26886a3b372beab
Daniel Pantoja <daanielpantoja@gmail.com>
2023-06-02 15:40:26 +0200

    SumaDescendent y documentación

--- Algoritmos/src/es/iesmz/ed/algoritmos/SumaDescendent.java
+++ Algoritmos/src/es/iesmz/ed/algoritmos/SumaDescendent.java
@@ -1,14 +1,30 @@
 package es.iesmz.ed.algoritmos;
 
+/**
+ * @Author: Daniel Pantoja Cedeño
+ * @Version: 1.0
+ */
 public class SumaDescendent {
-    public static int sumaDescendent(int digit) {
-        int digit = 123;
-
-        String digitStr = String.valueOf(digit);
-        int sumaDesc = 0;
-
-        for (int i = 0; i < digitStr.length(); i++) {
-            sumaDesc += Integer.parseInt(digitStr.substring(i));
+    /**
+     *
+     * @param digit El número que va a usarse en el algoritmo
+     * @return El resultado del algoritmo
+     */
+    public static long sumaDescendent(long digit) {
+//        devuelve -1 si el numero es 0 o negativo
+        if (digit <= 0) {
+            return -1;
         }
-        System.out.println(sumaDesc);
+
+//        declaración de las variables de uso
+        String digitStr = String.valueOf(digit);
+        long sumaDesc = 0;
+
+//        el bucle recorre toda la cadena y suma cada carácter convertido en numero en sumaDesc
+        for (int i = 0; i < digitStr.length(); i++) {
+            sumaDesc += Character.getNumericValue(digitStr.charAt(i));
+        }
+//        devuelve el sumatorio
+        return sumaDesc;
+    }
 }
--- Algoritmos/src/es/iesmz/ed/algoritmos/SumaDescendentTest.java
+++ Algoritmos/src/es/iesmz/ed/algoritmos/SumaDescendentTest.java
@@ -8,63 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class SumaDescendentTest {
     @ParameterizedTest
     @CsvSource({
-            "1234,false",
-            "22222,true",
-            "0,false",
-            "2,true",
-            "444,true",
-            "200,true",
-            "888888,true",
-            "24680,true",
-            "13579,false",
-            "111111,false",
-            "987654,false",
-            "100000,false",
-            "7777777,false",
-            "123456789,false",
-            "666666,true",
-            "222,true",
-            "800,true",
-            "555,false",
-            "2222,true",
-            "44444,true",
-            "123,false",
-            "9999,false",
-            "246,true",
-            "111,false",
-            "202020,true",
-            "56789,false",
-            "33333,false",
-            "400,true",
-            "666,true",
-            "1001,false",
-            "7777,false",
-            "26664440,true",
-            "888,true",
-            "333,false",
-            "1111,false",
-            "24681357,false",
-            "2222222,true",
-            "777777,false",
-            "101,false",
-            "8888888,true",
-            "121,false",
-            "4444,true",
-            "500,false",
-            "999999,false",
-            "11111,false",
-            "222222,true",
-            "999,false",
-            "135,false",
-            "86420,true",
-            "4444444,true",
-            "369,false",
-            "1010101,false",
-            "12345678,false"
+            "12345, 15085",
+            "12, 14",
+            "5423, 5872",
+            "987654, 1083674",
+            "0, -1",
+            "1234147234,1506790910",
+            "-1234123, -1"
     })
 
-    void SumaDescendent(int input, int resultadoEsperado) {
-        int resultadoReal = SumaDescendent.sumaDescendent(input);
+    void SumaDescendent(long input, long resultadoEsperado) {
+        long resultadoReal = SumaDescendent.sumaDescendent(input);
         assertEquals(resultadoEsperado, resultadoReal);
     }
 }
